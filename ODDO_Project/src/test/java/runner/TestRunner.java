f0cc0b51ed4b6d24bb52a8c391427c508d0eaffe
waelgharsalliii <95108908+waelgharsalliii@users.noreply.github.com>
2025-03-17 04:08:14 +0100

    Update TestRunner.java

--- ODDO_Project/src/test/java/runner/TestRunner.java
+++ ODDO_Project/src/test/java/runner/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
    features = "src/test/resources/AIO_Features",   // Path to the feature files
    glue = "/src/main/java/com/exemple/oddoproject/bdd",           // Path to the step definitions
-   plugin = {"pretty", "json:target/cucumber.json"}
+   plugin = {"pretty", "html:target/cucumber-Report.html","json:target/report.json"}
 )
 public class TestRunner {
 	
